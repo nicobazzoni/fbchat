a6d66557e666facccda822c3f53035adc21cb845
nico bazzoni <nicobazzoni@gmail.com>
2022-11-16 15:18:31 -0500

    cleint and server swapped loading

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import ChatInput from './ChatInput'
 import { Message } from './MessageList'
 
 async function HomePage() {
-    const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then((res) => res.json())
+    const data = await fetch(`${process.env.VERCEL_URL || "http://localhost:3000"}/api/getMessages`).then((res) => res.json())
     console.log(data)
 
     const messages: Message[] = data.messages
